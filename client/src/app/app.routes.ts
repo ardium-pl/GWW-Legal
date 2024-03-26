1aa4333f84f9d651e6ea1ff73677161f1a34b5ae
joker876 <joker876x@gmail.com>
2024-03-26 19:08:31 +0100

    install material & create nsa page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { NsaPage } from './pages/nsa/nsa.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '/nsa', component: NsaPage },
+    { path: '*', redirectTo: '/nsa' },
+];
---
+++ client/src/app/pages/nsa/nsa.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nsa',
+  standalone: true,
+  imports: [],
+  templateUrl: './nsa.page.html',
+  styleUrl: './nsa.page.scss'
+})
+export class NsaPage {
+
+}
