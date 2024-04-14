ee0792b8203af41f72fb326edec1496f06a0d2f7
joker876 <joker876x@gmail.com>
2024-04-14 20:19:31 +0200

    [server] add /nsa/additional_question endpoint

--- server/api/router.js
+++ server/api/router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 import { askGptAboutNSA, askNSA } from "./nsaMain.js";
 import { getCourtRuling } from "./scraper.js";
 
-router.post("/nsa/question", async (req, res) => {
+router.post("/nsa/questions", async (req, res) => {
   try {
     const { courtRuling, systemMessage, userMessages } = req.body;
     if (!courtRuling) {
@@ -15,12 +15,27 @@ router.post("/nsa/question", async (req, res) => {
     const result = await Promise.all(userMessages.map(async (message, i) => {
       const response = await askGptAboutNSA(systemMessage, message, courtRuling);
       progress[i] = true;
-      res.write({progress, responses: null});
+      res.write({ progress, responses: null });
       return response;
-    })); 
-    
+    }));
+
     res.write({ progress, responses: result });
     res.end();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+});
+
+router.post("/nsa/additional_question", async (req, res) => {
+  try {
+    const { courtRuling, systemMessage, userMessage } = req.body;
+    if (!courtRuling) {
+      return res.status(400).send({ error: "Court ruling is required." });
+    }
+
+    const response = await askGptAboutNSA(systemMessage, userMessage, courtRuling);
+    res.status(200).json(response);
   } catch (error) {
     console.error(error);
     res.status(500).send({ error: "Internal Server Error" });
@@ -33,8 +48,6 @@ router.post("/nsa/query", async (req, res) => {
     if (!caseSignature) {
       return res.status(400).send({ error: "Case signature is required." });
     }
-
-    console.log("got the request", caseSignature);
 
     const result = await getCourtRuling(caseSignature);
     res.json(result);
