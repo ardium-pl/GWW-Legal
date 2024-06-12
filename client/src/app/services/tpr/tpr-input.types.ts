97ed2c4dc3952d63741de5cc90e4d51dcdf91c9c
Paweł Bakalarz <joker876x@gmail.com>
2024-06-12 15:49:45 +0200

    copy over tpr service

---
+++ client/src/app/services/tpr/tpr-input.types.ts
@@ -0,0 +1,23 @@
+export type TPR_input ={
+    periodFrom:string;
+    periodUntil:string;
+    taxID: string;
+    fullName: string;
+    countryCode: string;
+    pkdCode: string;
+    taxCategory: 'ZK01' | 'ZK02';
+    operatingMargin: number;
+    profitMargin: number;
+    returnOnAssets: number;
+    returnOnEquity: number;
+    transactions: Array<transaction>
+}
+
+type transaction = {
+    transactionCategory: string;
+    subjectMatter: string;
+    transactionValue: number;
+    currencyCode: string;
+    correction: 'KC01' | 'KC02';
+    compensation: 'KS01' | 'KS02' | 'KS03';
+}
