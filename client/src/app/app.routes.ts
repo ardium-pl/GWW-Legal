3c19fc413c9f1e3aefe5e49601f896019a5456c1
Jakub <brodka.jakub@gmail.com>
2024-07-30 21:38:59 +0200

    GWW main page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { NsaPage } from './pages/nsa/nsa.page';
 import { TprPage } from './pages/tpr/tpr.page';
+import { MainPage } from './pages/main/main.page';
 
 export const routes: Routes = [
+  { path: '', component: MainPage},
   { path: 'nsa', component: NsaPage },
   { path: 'tpr', component: TprPage },
-  { path: '**', redirectTo: 'nsa' },
+  { path: '**', redirectTo: '' },
 ];
---
+++ client/src/app/pages/main/main.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CardComponent } from 'app/components/card/card.component';
+
+@Component({
+  selector: '_main-page',
+  standalone: true,
+  imports: [RouterModule, CardComponent],
+  templateUrl: './main.page.html',
+  styleUrl: './main.page.scss',
+})
+export class MainPage {}
